6e389fe862139d185481c8899df59e046d7d6447
Oğuzcan Kandemir <90668203+oguzcankandemir@users.noreply.github.com>
2022-11-21 22:54:26 +0300

    fix

---
+++ Html_Exzample/Content/js/JavaScript.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        items: 3,
+        slideBy: 2,
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1,
+                loop: false,
+                autoplay: false,
+                dots: true
+            },
+            992: {
+                items: 2
+            },
+            1200: {
+                items: 3
+            }
+        }
+    })
+})
---
+++ Html_Exzample/Content/js/odev.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function ($) {
+    $(".title").click(function () {
+
+        var cont = $(this).parents("article");
+        $('.content').slideUp(300);
+        var active = cont.find('.title');
+        $(".title").removeClass("active");
+
+
+        if (active.hasClass('active')) {
+            active.removeClass('active');
+        } else {
+            active.addClass('active');
+        }
+
+        var detail = cont.find('.content');
+        if (detail.is(':hidden')) {
+
+            detail.slideDown(300);
+        } else {
+            detail.slideUp(300);
+            active.removeClass('active');
+        }
+    });
+})
